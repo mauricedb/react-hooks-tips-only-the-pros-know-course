30230a7f4ff472fcbad139fed464113734f2a089
Maurice de Beijer <maurice.de.beijer@gmail.com>
2021-05-19 16:36:34 +0200

    Added Prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
-import eslint from "@rollup/plugin-eslint";
+import { defineConfig } from "vite"
+import reactRefresh from "@vitejs/plugin-react-refresh"
+import eslint from "@rollup/plugin-eslint"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  { ...eslint({ include: "src/**/*.+(js|jsx|ts|tsx)" }), enforce: "pre" },
-  plugins: [reactRefresh()]
+  plugins: [
+    { ...eslint({ include: "src/**/*.+(js|jsx|ts|tsx)" }), enforce: "pre" },
+    reactRefresh(),
+  ],
 })
