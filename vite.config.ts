051000305f09932d4a676fbbc37c86cdecf57c64
Maurice de Beijer <maurice.de.beijer@gmail.com>
2021-05-19 16:26:50 +0200

    Added ESlint

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import eslint from "@rollup/plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  { ...eslint({ include: "src/**/*.+(js|jsx|ts|tsx)" }), enforce: "pre" },
   plugins: [reactRefresh()]
 })
