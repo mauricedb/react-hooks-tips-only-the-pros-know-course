dd373820a81bcb0d2b8ef3643c11c278b1ca9227
Maurice de Beijer <maurice.de.beijer@gmail.com>
2021-06-18 18:04:22 +0200

    LabeledInput with errors

--- src/components/LabeledInput.tsx
+++ src/components/LabeledInput.tsx
@@ -1,25 +1,24 @@
-import React, { InputHTMLAttributes, ReactElement } from "react"
+import React, { forwardRef, InputHTMLAttributes, ReactElement } from "react"
 import classNames from "classnames"
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
   label: string
+  error?: string
 }
 
-export const LabeledInput = ({
-  id,
-  label,
-  className,
-  ...props
-}: Props): ReactElement => {
-  return (
-    <div className={classNames("form-group", className)}>
-      <label htmlFor={id} className="form-label">
-        {label}
-      </label>
+export const LabeledInput = forwardRef<HTMLInputElement, Props>(
+  ({ id, label, className, error, ...props }, ref): ReactElement => {
+    return (
+      <div className={classNames("form-group", className)}>
+        <label htmlFor={id} className="form-label">
+          {label}
+        </label>
 
-      <input {...props} id={id} className="form-control" />
-    </div>
-  )
-}
+        <input {...props} id={id} className="form-control" ref={ref} />
+        {error ? <div className="invalid-feedback d-block">{error}</div> : null}
+      </div>
+    )
+  }
+)
 
 LabeledInput.displayName = "LabeledInput"
